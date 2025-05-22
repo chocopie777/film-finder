00f026646911d83288872ade46af9bc3858e33cb
chocopie777 <egoran99@gmail.com>
2025-05-22 15:07:19 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <main>
+    <div class="mx-auto max-w-[1200px]">
+
+    </div>
+  </main>
+  <Footer />
+</template>
+
+<style scoped>
+</style>
